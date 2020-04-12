36feae63f0d8791fb8ad17537aad94f802577633
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-04-12 04:22:33 -0300

    Section 3: class 40. Handling Events with Methods

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from "react";
+import Person from "./Person/Person";
+
+import "./App.css";
 
 export default class App extends Component {
+  state = {
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
+    ],
+  };
+
+  switchNameHandler = () => {
+    console.log('Was clicked')
+  }
+
   render() {
     return (
       <>
-        <h1>Olá Mundo</h1>
+      { console.log(this.state)}
+        <div className="App">
+          <h1>Hi, I'm React App</h1>
+          <h1>This is really working!</h1>
+          <button onClick={ this.switchNameHandler }>Switch Name</button>
+          <Person  name={this.state.persons[0].name} age={this.state.persons[0].age}>
+            My Hobbies: Teaching
+          </Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+            My Hobbies: Racing
+          </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+            My Hobbies: Ciclism
+          </Person>
+        </div>
       </>
     );
   }
