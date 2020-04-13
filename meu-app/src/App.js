02e9d41e72b848848506adf69cfee3e3bbbef84b
MatheusEstanislau <matheus.estanislau7@gmail.com>
2020-04-12 21:52:22 -0300

    Section 3. Class 42: Manipulating the State

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -13,24 +13,40 @@ export default class App extends Component {
   };
 
   switchNameHandler = () => {
-    console.log('Was clicked')
-  }
+    // console.log('Was clicked')
+    this.setState({
+      persons: [
+        { name: "Maximiliam", age: 23 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 26 },
+      ],
+    });
+  };
 
   render() {
     return (
       <>
-      { console.log(this.state)}
+        {console.log(this.state)}
         <div className="App">
           <h1>Hi, I'm React App</h1>
           <h1>This is really working!</h1>
-          <button onClick={ this.switchNameHandler }>Switch Name</button>
-          <Person  name={this.state.persons[0].name} age={this.state.persons[0].age}>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          >
             My Hobbies: Teaching
           </Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+          >
             My Hobbies: Racing
           </Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          >
             My Hobbies: Ciclism
           </Person>
         </div>
